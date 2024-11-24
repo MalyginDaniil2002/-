7d34098cedc68b772cbf684eaea918094affb79f
MalyginDaniil2002 <svetl@LAPTOP-R61RSI52.>
2024-11-24 23:58:29 +0300

    Исполняемые файлы перемещены в папки для разных ОС

---
+++ 02.Memory_Overcommit/Linux_codes/memory_overcommit_unix.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef unsigned int type_size;
+typedef long long type_array;
+#define INTERVAL 0.8
+#define LIMIT 2560
+
+int get_data(char *argv[], type_size* size, int* flag) {
+    *size = atoi(argv[1]);
+    *flag = atoi(argv[2]);
+}
+
+void overcommit(type_array *array, type_size size, int flag) {
+    type_size step = 4*1024/sizeof(array[0]);
+    type_array summa = 0;
+    type_size index = step;
+    while ((index < size) && (index > 0)) {
+        if (flag == 0)
+            summa += array[index];
+        else
+            array[index] = index;
+        if ((index / step) % LIMIT == 0) {
+            sleep(INTERVAL);
+        }
+        index += step;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int flag;
+    type_size size = 1;
+    size, flag = get_data(argv, &size, &flag);
+    printf("size = %u, flag = %i\n", size, flag);
+    type_array *array = malloc(sizeof(type_array)*size);
+    overcommit(array, size, flag);
+    printf("STOP!!!\n");
+    free(array);
+    return 0;
+}
